04aabf9f829cf4ef9148cc833b937f393072de16
NguyenDucMinhKhoi <123210036@sv1.dut.udn.vn>
2024-07-18 00:46:02 +0700

    Phong tro

---
+++ client/src/store/reducers/userReducer.js
@@ -0,0 +1,14 @@
+const initState = {
+    userData: {}
+}
+
+const userReducer = (state = initState) => {
+    switch (action.type) {
+
+
+        default:
+            return state;
+    }
+}
+
+export default userReducer
