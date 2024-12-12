35860d49dccca5b5fa9773d0dd6634bbb91c8c7f
NguyenDucMinhKhoi <123210036@sv1.dut.udn.vn>
2024-12-12 19:20:06 +0700

    Xử lí sự kiện onClick hoán đổi trạng thái đăng nhập và đăng kí

--- client/src/containers/public/header.js
+++ client/src/containers/public/header.js
@@ -2,38 +2,40 @@ import React, { useCallback } from 'react'
 import logo from '../../Assets/LogoHome.png'
 import { Button } from '../../components'
 import icons from '../../utils/icons'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Link } from 'react-router-dom'
 import { path } from '../../utils/constant'
 
 const { AiOutlinePlusCircle } = icons
 
 const Header = () => {
   const navigate = useNavigate()
-  const goLoin = useCallback(() => {
-    navigate(path.LOGIN)
-  })
+  const goLogin = useCallback((flag) => {
+    navigate(path.LOGIN, { state: { flag } })
+  }, [])
 
   return (
     <div className="w-1100">
       <div className='w-full flex items-center justify-between'>
-        <img
-          src={logo}
-          alt='logo'
-          className='w-[60px] h-[60px] object-contain'
-        />
+        <Link to={'/'}>
+          <img
+            src={logo}
+            alt='logo'
+            className='w-[60px] h-[60px] object-contain'
+          />
+        </Link>
         <div className='flex items-center gap-1'>
           <small>Hello!</small>
           <Button
             text={'Login'}
             textColor='text-white'
             bgColor='bg-[#3961fb]'
-            onClick={goLoin}
+            onClick={() => goLogin(false)}
           />
           <Button
             text={'Sign up'}
             textColor='text-white'
             bgColor='bg-[#3961fb]'
-            onClick={goLoin}
+            onClick={() => goLogin(true)}
           />
           <Button
             text={'Post new news'} textColor='text-white' bgColor='bg-secondary2'
--- client/src/containers/public/login.js
+++ client/src/containers/public/login.js
@@ -1,23 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { InputFrom, Button } from '../../components'
+// import { Link } from 'react-router-dom'
+import { useLocation } from 'react-router-dom'
 
 const Login = () => {
+  const location = useLocation()
+
+  const [isRegister, setIsRegister] = useState(location.state?.flag)
+  useEffect(() => {
+    setIsRegister(location.state?.flag)
+  }, [location.state?.flag])
+
   return (
     <div className='login-container my-3 bg-white w-[600px] p-[30px] pb-[100px] rounded-md shadow-sm'>
-      <h3 className='font-semibold text-2xl md-3'>Login</h3>
+      <h3 className='font-semibold text-2xl md-3'>{isRegister ? 'Sign up' : 'Login'}</h3>
       <div className='w-full flex flex-col gap-3'>
+        {isRegister && <InputFrom label={'Full Name'} />}
         <InputFrom label={'Phone Number'} />
         <InputFrom label={'Password'} />
         <Button
-          text='Login'
+          text={isRegister ? 'Sign up' : 'Login'}
           bgColor='bg-secondary1'
           textColor='text-white'
           fullWidth
         />
       </div>
       <div className='mt-7 flex items-center justify-between'>
-        <small className='text-[blue] hover:text-[red] cursor-pointer'>Forgot your password?</small>
-        <small className='text-[blue] hover:text-[red] cursor-pointer'>Sign up</small>
+        {isRegister
+          ? <small> Already have an account? <span
+            onClick={() => { setIsRegister(false) }}
+            className='text-blue-500 hover:underline cursor-pointer'
+          >
+            Login now
+          </span></small>
+          : <>
+            <small className='text-[blue] hover:text-[red] cursor-pointer'>Forgot your password?</small>
+            <small
+              onClick={() => { setIsRegister(true) }}
+              className='text-[blue] hover:text-[red] cursor-pointer'
+            >
+              Sign up
+            </small>
+          </>
+        }
       </div>
     </div>
   )
