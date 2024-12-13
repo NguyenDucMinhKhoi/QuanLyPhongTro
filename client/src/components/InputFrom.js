def2288170e94c045d7050f25e9c6d35e0e5561a
NguyenDucMinhKhoi <123210036@sv1.dut.udn.vn>
2024-12-13 23:58:56 +0700

    Xử lí việc đăng kí tài khoản với API và lưu token dưới localstorage bằng redux-persist

--- client/src/components/InputFrom.js
+++ client/src/components/InputFrom.js
@@ -1,6 +1,6 @@
 import React, { memo } from 'react'
 
-const InputFrom = ({ label }) => {
+const InputFrom = ({ label, value, setValue, type }) => {
   return (
     <div>
       <label htmlFor="phone" className='text-xs'>{label}</label>
@@ -8,6 +8,8 @@ const InputFrom = ({ label }) => {
         type="text"
         id="phone"
         className='outline-none bg-[#e8f0fe] p-2 rounded-md w-full'
+        value={value}
+        onChange={(e) => setValue(prev =>({...prev, [type]: e.target.value}))} 
       />
     </div>
   )
--- client/src/containers/public/login.js
+++ client/src/containers/public/login.js
@@ -1,28 +1,44 @@
 import React, { useState, useEffect } from 'react'
 import { InputFrom, Button } from '../../components'
-// import { Link } from 'react-router-dom'
+import { apiRegister } from '../../services/auth'
 import { useLocation } from 'react-router-dom'
+import * as actions from '../../store/actions'
+import { useDispatch } from 'react-redux'
 
 const Login = () => {
   const location = useLocation()
+  const dispatch = useDispatch()
 
   const [isRegister, setIsRegister] = useState(location.state?.flag)
+  const [payload, setPayload] = useState({
+    phone: '',
+    password: '',
+    name: ''
+  })
   useEffect(() => {
     setIsRegister(location.state?.flag)
   }, [location.state?.flag])
+
+  const handleSubmit = async () => {
+    console.log(payload)
+    dispatch(actions.register(payload))
+    // console.log(response)
+  }
 
   return (
     <div className='login-container my-3 bg-white w-[600px] p-[30px] pb-[100px] rounded-md shadow-sm'>
       <h3 className='font-semibold text-2xl md-3'>{isRegister ? 'Sign up' : 'Login'}</h3>
       <div className='w-full flex flex-col gap-3'>
-        {isRegister && <InputFrom label={'Full Name'} />}
-        <InputFrom label={'Phone Number'} />
-        <InputFrom label={'Password'} />
+        {isRegister && <InputFrom label={'Full Name'} value={payload.name} setValue={setPayload} type={'name'} />}
+        <InputFrom label={'Phone Number'} value={payload.phone} setValue={setPayload} type={'phone'} />
+        <InputFrom label={'Password'} value={payload.password} setValue={setPayload} type={'password'} />
         <Button
           text={isRegister ? 'Sign up' : 'Login'}
           bgColor='bg-secondary1'
           textColor='text-white'
           fullWidth
+          onClick={handleSubmit}
+
         />
       </div>
       <div className='mt-7 flex items-center justify-between'>
