514c877c1c705822b9450de6910c281bba4744c8
NguyenDucMinhKhoi <123210036@sv1.dut.udn.vn>
2025-04-08 17:02:00 +0700

    Xây dựng chức năng tạo mới bài đăng-layout chính

--- client/src/utils/menuManage.js
+++ client/src/utils/menuManage.js
@@ -6,13 +6,13 @@ const menuManage = [
   {
     id: 1,
     text: "Đăng tin cho thuê",
-    path: "/he-thong/tao-moi-bai-dang",
+    path: "/he-thong/tao-moi-tin-dang",
     icon: <ImPencil2 />,
   },
   {
     id: 2,
     text: "Quản lý tin đăng",
-    path: "/he-thong/quan-ly-bai-dang",
+    path: "/he-thong/quan-ly-tin-dang",
     icon: <MdOutlineLibraryBooks />,
   },
   {
